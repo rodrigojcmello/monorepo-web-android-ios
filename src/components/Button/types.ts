9120746b489dbeefc76e1dfb1934c24100fcd1db
Rodrigo Mello <rodrigojcmello@gmail.com>
2019-11-03 13:45:04 -0300

    login

--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -1,7 +1,10 @@
 import { FormEvent } from 'react';
+import { NativeSyntheticEvent, NativeTouchEvent } from 'react-native';
 
 export interface ButtonProps {
   text: string;
   type?: 'submit' | 'button';
-  onPress: (event: FormEvent<HTMLButtonElement>) => void;
+  onPress: (
+    event: FormEvent<HTMLButtonElement> | NativeSyntheticEvent<NativeTouchEvent>
+  ) => void;
 }
